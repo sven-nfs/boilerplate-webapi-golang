0f9067c95f66e6bc8449576ef7820ff47569850e
Sven Groh <sven.groh@n-finity.solutions>
2017-09-12 19:25:25 +0200

    adds some basic public routes

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func main() {
 	//add a index route to the router
 	router.HandleFunc("/", Index)
 
+	//add basic public routes
+	router.HandleFunc("/public/models", ModelIndex)
+	router.HandleFunc("/public/models/{modelId}", ModelDetail)
+
 	//listen and serve
 	//log errors
 	log.Fatal(http.ListenAndServe(":8080", router))
@@ -23,5 +27,15 @@ func main() {
 
 // Index - handle function for index route
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
+	fmt.Fprintln(w, "welcome!")
+}
+
+func ModelIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "list of all models")
+}
+
+func ModelDetail(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	modelId := vars["modelId"]
+	fmt.Fprintln(w, "model:", modelId)
 }
