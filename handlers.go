c5cc76d042e96651071e860423afc538f3d74ccf
Sven Groh <sven.groh@n-finity.solutions>
2017-09-13 12:54:52 +0200

    takes care of some header info

--- handlers.go
+++ handlers.go
@@ -16,12 +16,20 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 // ModelIndex - handle function for public model route
 func ModelIndex(w http.ResponseWriter, r *http.Request) {
+
+	//grab this data from a db or whatever
 	ml := ModelList{
 		Model{ID: "0815-4223", Name: "random model 1", CreationDate: time.Now()},
 		Model{ID: "007-BOND", Name: "random model 2", CreationDate: time.Now()},
 	}
 
-	json.NewEncoder(w).Encode(ml)
+	//set some header info
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(ml); err != nil {
+		panic(err)
+	}
 }
 
 // ModelDetail - handle function for public model detail route
